7670c3ee97c5b220bceae1c7ab795d865be7627b
Mahima Jha <47323013+mahimajha@users.noreply.github.com>
2023-11-17 12:33:33 +0530

    Update streamlit_poc.py

--- streamlit_poc.py
+++ streamlit_poc.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas
+import requests
 
 st.title("My first page")
 st.header("🥗 Breakfast Menu")
@@ -16,5 +17,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 st.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 
