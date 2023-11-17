ca6b9be5ed2c309e32aceed1bf37d272cb5fe77d
Mahima Jha <47323013+mahimajha@users.noreply.github.com>
2023-11-17 16:59:43 +0530

    Update streamlit_poc.py

--- streamlit_poc.py
+++ streamlit_poc.py
@@ -26,7 +26,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
 st.dataframe(fruityvice_normalised)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
