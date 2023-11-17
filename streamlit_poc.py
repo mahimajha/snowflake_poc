0cf6df61dd98c8e9908a3229fe81ede7741ed658
Mahima Jha <47323013+mahimajha@users.noreply.github.com>
2023-11-17 17:09:42 +0530

    Update streamlit_poc.py

--- streamlit_poc.py
+++ streamlit_poc.py
@@ -33,7 +33,7 @@ my_data_row = my_cur.fetchall()
 st.header("The fruit load list contains")
 st.dataframe(my_data_row)
 
-added_fruit = st.text_input("What fruit would you like to add?",jackfruit)
+added_fruit = st.text_input("What fruit would you like to add?","jackfruit")
 st.write("Thanks for chhosing ", added_fruit)
 
 
